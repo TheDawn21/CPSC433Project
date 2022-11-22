a8428bbc9c050fe2e6cb28a6fae6ec408e80b234
Kaynen Mitchell <kaynen@live.ca>
2022-11-21 22:45:13 -0700

    Add most ArrayLists in Parser

--- CPSC433Project/src/Parser.java
+++ CPSC433Project/src/Parser.java
@@ -5,6 +5,18 @@ public class Parser {
     String name;
 
     // All parser arrays on pictures
+    ArrayList<Slot> m_game_slots;
+    ArrayList<Slot> t_game_slots;
+    ArrayList<Slot> m_prac_slots;
+    ArrayList<Slot> t_prac_slots;
+    ArrayList<Slot> f_prac_slots;
+    ArrayList<Event> games;
+    ArrayList<Event> practices;
+    // AL of not compatible
+    // AL of unwanted
+    // AL of prefered
+    // AL of pair
+    // AL of part assign
 
     public Parser(String filename) {
 
