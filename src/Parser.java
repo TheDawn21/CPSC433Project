4c2b082392ca7b210bb1268db34145e7d56f7868
Kaynine-the-Robot <kaynen@live.ca>
2022-11-25 17:38:34 -0700

    Added Parser functionality for parsing Name and GameSlots

--- src/Parser.java
+++ src/Parser.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Parser {
     
-    String name;
-
+    String exampleName;
+    File file;
+    Scanner scan;
     // All parser arrays on pictures
     ArrayList<Slot> m_game_slots;
     ArrayList<Slot> t_game_slots;
@@ -12,18 +16,101 @@ public class Parser {
     ArrayList<Slot> f_prac_slots;
     ArrayList<Event> games;
     ArrayList<Event> practices;
-    // AL of not compatible
-    // AL of unwanted
-    // AL of prefered
-    // AL of pair
-    // AL of part assign
+    ArrayList<NotCompatible> ncList;
+    ArrayList<Unwanted> unwants;
+    ArrayList<Prefered> prefers;
+    ArrayList<Pair> pairs;
+    ArrayList<PartAssign> paList;
 
-    public Parser(String filename) {
+    public Parser(String inFilename) {
+        openFile(inFilename);
+        try {
+            parse();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * Parse will read a stored file line by line, and parse the lines into 
+     * the appropriate arrays to pass on to the Tree Search
+     */
+    public void parse() throws FileNotFoundException {
+
+        String buffer = new String("");
+        scan = new Scanner(file);
+
+        while(scan.hasNextLine()){
+            String s = scan.nextLine();
+            if(s.length() == 0) {
+
+            }
+            else{
+
+            }
+        }
 
     }
 
-    public ArrayList<ArrayList<Class>> parse() {
-
-        return null;
+    public void openFile(String filename) {
+        try {
+            file = new File(filename);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
+
+    // Fill list will fill the correct array given a String buffer which are a section of the file
+    public void fillList(String buff) {
+        String[] lines = buff.split("\n");
+        if(lines.length <= 1) return;
+        // Switch based on the section head in the file
+        switch(lines[0]){
+            case "Name":
+                exampleName = lines[1];
+            case "Game slots":
+                for(int i = 1; i < lines.length; i++) {
+                    // Read each line and split them based on days
+                    String[] slotInfo = lines[i].split(",");
+                    // Monday specific slots
+                    if(slotInfo[0] == "MO"){
+                        String day = slotInfo[0];
+                        String startTime = slotInfo[1];
+                        Integer startHour = Integer.parseInt((slotInfo[1].split(":"))[0])+1;
+                        String endTime = (startHour).toString() +":"+ (slotInfo[1].split(":"))[1];
+                        int gameMax = Integer.parseInt(slotInfo[2]); 
+                        int gameMin = Integer.parseInt(slotInfo[3]); 
+                        // Special must be false on mondays
+                        m_game_slots.add(new Slot(day, startTime, endTime, gameMax, gameMin, false));
+                    }
+                    // Tuesday specific slots
+                    else if(slotInfo[0] == "TU") {
+                        // IF a game slot is 11-12:30 Tues/Thurs don't include in available slots?
+                        String day = slotInfo[0];
+                        String startTime = slotInfo[1];
+                        String endTime;
+                        Integer startHour = Integer.parseInt((slotInfo[1].split(":"))[0]);
+                        Integer startMins = Integer.parseInt((slotInfo[1].split(":"))[1]);
+                        boolean special = false;
+                        if(startHour == 11) special = true;
+                        if(startMins==30){
+                            startHour += 2;
+                            endTime = (startHour).toString() +":00";
+                        }
+                        else{
+                            startHour+=1;
+                            endTime = (startHour).toString() +":30";
+                        }
+                        int gameMax = Integer.parseInt(slotInfo[2]); 
+                        int gameMin = Integer.parseInt(slotInfo[3]); 
+                        m_game_slots.add(new Slot(day, startTime, endTime, gameMax, gameMin, special));
+                    }
+                }
+        }
+
+
+
+    }
+
 }
--- src/Slot.java
+++ src/Slot.java
@@ -1,19 +1,19 @@
 public class Slot {
     public String day; // Start day: "Monday", "Tuesday", or "Friday"
-    public int startTime;
-    public int endTime;
+    public String startTime;
+    public String endTime;
     public int max;
     public int min;
-    public boolean type;
+    //public boolean type; What this do?
     public boolean isSpecial;
 
-    public Slot (String day, int startTime, int endTime, int max, int min, boolean type, boolean isSpecial) {
+    public Slot (String day, String startTime, String endTime, int max, int min, boolean isSpecial) {
         this.day = day;
         this.startTime = startTime;
         this.endTime = endTime;
         this.max = max;
         this.min = min;
-        this.type = type;
+        //this.type = type;
         this.isSpecial = isSpecial;
     }
 }
