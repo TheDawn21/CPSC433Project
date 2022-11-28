32e041648bdd1fe841b7bf705bfcad949a3e182a
Kaynen Mitchell <kaynen@live.ca>
2022-11-28 11:21:08 -0700

    Add string trim to Parser.fillList for whitespace

--- src/Parser.java
+++ src/Parser.java
@@ -70,9 +70,14 @@ public class Parser {
             case "name":
                 exampleName = lines[1];
             case "game slots":
+                // Starts at 1 to skip header
                 for(int i = 1; i < lines.length; i++) {
                     // Read each line and split them based on days
                     String[] slotInfo = lines[i].split(",");
+                    // This should trim all whitespace around the line coming from spaces after commas
+                    for(int j = 0; i < slotInfo.length; i++) {
+                        slotInfo[j] = slotInfo[j].trim();
+                    }
                     // Monday specific slots
                     if(slotInfo[0] == "MO"){
                         String day = slotInfo[0];
@@ -110,6 +115,50 @@ public class Parser {
                     }
                 }
             case "practice slots":
+                // Starts at 1 to skip header
+                for(int i = 1; i < lines.length; i++) {
+                    // Read each line and split them based on days
+                    String[] slotInfo = lines[i].split(",");
+                    // This should trim all whitespace around the line coming from spaces after commas
+                    for(int j = 0; i < slotInfo.length; i++) {
+                        slotInfo[j] = slotInfo[j].trim();
+                    }
+                    // Monday specific slots
+                    if(slotInfo[0] == "MO"){
+                        String day = slotInfo[0];
+                        String startTime = slotInfo[1];
+                        Integer startHour = Integer.parseInt((slotInfo[1].split(":"))[0])+1;
+                        String endTime = (startHour).toString() +":"+ (slotInfo[1].split(":"))[1];
+                        int gameMax = Integer.parseInt(slotInfo[2]); 
+                        int gameMin = Integer.parseInt(slotInfo[3]); 
+                        // Special must be false on mondays
+                        m_game_slots.add(new Slot(day, startTime, endTime, gameMax, gameMin, false));
+                    }
+                    // Tuesday specific slots
+                    else if(slotInfo[0] == "TU") {
+                        // IF a game slot is 11-12:30 Tues/Thurs don't include in available slots?
+                        String day = slotInfo[0];
+                        String startTime = slotInfo[1];
+                        String endTime;
+                        Integer startHour = Integer.parseInt((slotInfo[1].split(":"))[0]);
+                        Integer startMins = Integer.parseInt((slotInfo[1].split(":"))[1]);
+                        boolean special = false;
+                        // Account for a special game slot, the only special game slot
+                        if(startHour == 11) special = true;
+                        // MAke the end time for which the minutes will flip and hour will be 1 or 2 hours ahead
+                        if(startMins==30){
+                            startHour += 2;
+                            endTime = (startHour).toString() +":00";
+                        }
+                        else{
+                            startHour+=1;
+                            endTime = (startHour).toString() +":30";
+                        }
+                        int gameMax = Integer.parseInt(slotInfo[2]); 
+                        int gameMin = Integer.parseInt(slotInfo[3]); 
+                        m_game_slots.add(new Slot(day, startTime, endTime, gameMax, gameMin, special));
+                    }
+                }
             case "games":
             case "practices":
             case "not compatible":
