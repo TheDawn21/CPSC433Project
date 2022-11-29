b6098acc54475e2935bb4140ddc9b7cdcb8d6603
Kaynine-the-Robot <kaynen@live.ca>
2022-11-28 21:03:17 -0700

    Add Not Compatible case to Parser.fillList

--- src/Parser.java
+++ src/Parser.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Parser {
     
@@ -16,11 +17,12 @@ public class Parser {
     ArrayList<Slot> f_prac_slots;
     ArrayList<Event> games;
     ArrayList<Event> practices;
-    ArrayList<NotCompatible> ncList;
-    ArrayList<Unwanted> unwants;
-    ArrayList<Prefered> prefers;
-    ArrayList<Pair> pairs;
-    ArrayList<PartAssign> paList;
+    // Hashmaps
+    HashMap<Event, ArrayList<Event>> ncMap;// = new HashMap<Event, ArrayList<Event>>();
+    HashMap<Event, ArrayList<Slot>> unwantMap;
+    HashMap<Event, ArrayList<Object[]>> preferMap;
+    HashMap<Event, ArrayList<Event>> pairMap;
+    HashMap<Event, ArrayList<Event>> paMap;
 
     public Parser(String inFilename) {
         openFile(inFilename);
@@ -79,7 +81,7 @@ public class Parser {
                         slotInfo[j] = slotInfo[j].trim();
                     }
                     // Monday specific slots
-                    if(slotInfo[0] == "MO"){
+                    if(slotInfo[0].equals("MO")){
                         String day = slotInfo[0];
                         String startTime = slotInfo[1];
                         Integer startHour = Integer.parseInt((slotInfo[1].split(":"))[0])+1;
@@ -91,7 +93,7 @@ public class Parser {
                         m_game_slots.add(new Slot(day, startTime, endTime, gameMax, gameMin, special));
                     }
                     // Tuesday specific slots
-                    else if(slotInfo[0] == "TU") {
+                    else if(slotInfo[0].equals("TU")) {
                         // IF a game slot is 11-12:30 Tues/Thurs don't include in available slots?
                         String day = slotInfo[0];
                         String startTime = slotInfo[1];
@@ -102,7 +104,7 @@ public class Parser {
                         // Account for a special game slot, the only special game slot
                         if(startHour == 11) special = true;
                         // MAke the end time for which the minutes will flip and hour will be 1 or 2 hours ahead
-                        if(startMins==30){
+                        if(startMins == 30){
                             startHour += 2;
                             endTime = (startHour).toString() +":00";
                         }
@@ -125,7 +127,7 @@ public class Parser {
                         slotInfo[j] = slotInfo[j].trim();
                     }
                     // Monday specific slots
-                    if(slotInfo[0] == "MO"){
+                    if(slotInfo[0].equals("MO")){
                         String day = slotInfo[0];
                         String startTime = slotInfo[1];
                         Integer startHour = Integer.parseInt((slotInfo[1].split(":"))[0])+1;
@@ -137,7 +139,7 @@ public class Parser {
                         m_prac_slots.add(new Slot(day, startTime, endTime, gameMax, gameMin, special));
                     }
                     // Tuesday specific slots
-                    else if(slotInfo[0] == "TU") {
+                    else if(slotInfo[0].equals("TU")) {
                         // IF a game slot is 11-12:30 Tues/Thurs don't include in available slots?
                         String day = slotInfo[0];
                         String startTime = slotInfo[1];
@@ -151,7 +153,7 @@ public class Parser {
                         t_prac_slots.add(new Slot(day, startTime, endTime, gameMax, gameMin, special));
                     }
                     // Friday specific slots
-                    else if(slotInfo[0] == "FR") {
+                    else if(slotInfo[0].equals("FR")) {
                         // IF a game slot is 11-12:30 Tues/Thurs don't include in available slots?
                         String day = slotInfo[0];
                         String startTime = slotInfo[1];
@@ -173,7 +175,7 @@ public class Parser {
                     String ageAndTier = slotInfo[1];
                     String age;
                     String tier;
-                    if(ageAndTier.charAt(0) == 'O') {
+                    if(ageAndTier.charAt(0) == 'O') { //Idk if char needs to be .equals
                         age = "O18";
                         tier = "T0"; // DEFAULT FOR NOW, SUBJECT TO CHANGE
                     }
@@ -197,7 +199,7 @@ public class Parser {
                     String ageAndTier = slotInfo[1];
                     String age;
                     String tier;
-                    if(ageAndTier.charAt(0) == 'O') {
+                    if(ageAndTier.charAt(0) == 'O') { //Idk if char needs to be .equals
                         age = "O18";
                         tier = "T0"; // DEFAULT FOR NOW, SUBJECT TO CHANGE
                     }
@@ -209,7 +211,7 @@ public class Parser {
                     // Dealing with division, for practices div can be in different positions
                     String div = "";
                     for(int j = 0; i < slotInfo.length; i++) {
-                        if (slotInfo[j] == "DIV") {
+                        if (slotInfo[j].equals("DIV")) {
                             div = slotInfo[j+1]; // Should be safe, always next entry
                         }
                     }
@@ -220,14 +222,85 @@ public class Parser {
                     practices.add(new Event(lines[i], slotInfo[0], age, tier, divFinal, pracOrGame));
                 }
             case "not compatible":
+                for(int i = 1; i < lines.length; i++) {
+                    // Read each line and assign them into the games list
+                    String[] slotInfo = lines[i].split(",");
+                    boolean isPractice = false;
+                    String eventName1 = slotInfo[0];
+                    String eventName2 = slotInfo[1];
+                    Event event1 = null;
+                    Event event2 = null;
+                    // Handle the first event, find the reference in games or practices
+                    String[] eventInfo1 = eventName1.split(" ");
+                    for(int k = 0; k < eventInfo1.length; k++) {
+                        if(eventInfo1[k].equals("PRC") || eventInfo1[k].equals("OPN")) {
+                            isPractice = true;
+                        }
+                    }
+                    if(isPractice) {
+                        for(int j = 0; j < practices.size(); j++) {
+                            if(practices.get(j).name.equals(eventName1) ) {
+                                event1 = practices.get(j);
+                            }
+                        }
+                    }
+                    else {
+                        for(int j = 0; j < games.size(); j++) {
+                            if(games.get(j).name.equals(eventName1) ) {
+                                event1 = games.get(j);
+                            }
+                        }
+                    }
+                    isPractice = false; // Make sure to reset this value
+                    // Handle the second event, find the reference in games or practices
+                    String[] eventInfo2 = eventName2.split(" ");
+                    for(int k = 0; k < eventInfo2.length; k++) {
+                        if(eventInfo2[k].equals("PRC") || eventInfo2[k].equals("OPN")) {
+                            isPractice = true;
+                        }
+                    }
+                    if(isPractice) {
+                        for(int j = 0; j < practices.size(); j++) {
+                            if(practices.get(j).name.equals(eventName2) ) {
+                                event2 = practices.get(j);
+                            }
+                        }
+                    }
+                    else {
+                        for(int j = 0; j < games.size(); j++) {
+                            if(games.get(j).name.equals(eventName1) ) {
+                                event2 = games.get(j);
+                            }
+                        }
+                    }
+                    // Check for if one of the events does not exist in games/practices
+                    if(event1 == null || event2 == null) {
+                        System.out.println("Event does not exist in possible Events");
+                        System.exit(0);
+                    }
+                    // Add hashmap entry for event 1 -> event 2
+                    if(ncMap.get(event1) == null) {
+                        ArrayList<Event> mapVal = new ArrayList<Event>();
+                        mapVal.add(event2);
+                        ncMap.put(event1, mapVal);
+                    }
+                    else {
+                        ncMap.get(event1).add(event2);
+                    }
+                    // Add hashmap entry for event 2 -> event 1
+                    if(ncMap.get(event2) == null) {
+                        ArrayList<Event> mapVal = new ArrayList<Event>();
+                        mapVal.add(event1);
+                        ncMap.put(event2, mapVal);
+                    }
+                    else {
+                        ncMap.get(event2).add(event1);
+                    }
+                }
             case "unwanted":
             case "preferences":
             case "pair":
             case "partial assignments":
         }
-
-
-
     }
-
 }
--- src/Prefered.java
+++ src/Prefered.java
@@ -1,3 +1,5 @@
 public class Prefered {
-    
+    Slot slot;
+    Event event;
+    int prefValue;
 }
