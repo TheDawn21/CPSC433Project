2911fecedfb2b5f416435497b5c8a871361c086f
dustinn <binhng1908@gmail.com>
2022-11-30 01:13:29 -0700

    update tree

--- src/TreeSearch.java
+++ src/TreeSearch.java
@@ -19,13 +19,18 @@ public class TreeSearch {
 
         // Initialize eval
         // this.eval = new Eval(input);
+        // Initialize stack
+        this.stack = new Stack<>();
+
+        // Initalize best schedule
+        bestSched = new Schedule(null, null, null, null, Integer.MAX_VALUE);
 
         // Group slot
         gSlots = new ArrayList<>();
         gSlots.addAll(input.m_game_slots);
         // gSlots.addAll(input.t_game_slots);
-        // pSlots = new ArrayList<>();
-        // pSlots.addAll(input.m_prac_slots);
+        pSlots = new ArrayList<>();
+        pSlots.addAll(input.m_prac_slots);
         // pSlots.addAll(input.t_prac_slots);
         // pSlots.addAll(input.f_prac_slots);
 
@@ -41,42 +46,65 @@ public class TreeSearch {
         startTreeSearch(root);
     }
 
-    public void startTreeSearch(Schedule sched) {
+    private void startTreeSearch(Schedule sched) {
         specialAssign(sched);
         partAssign(sched);
 
-        // stack.add(sched);
-        // while (stack.size() != 0) {
-        //     sched = stack.pop();
-        //     sched.gamesLeft.get(0);
-        //     for (Slot gSlot : gSlots) {
-                
-        //         sched = new Schedule(sched);
-        //     }
-        // }
+        stack.add(sched);
+        while (stack.size() != 0) {
+            sched = stack.pop();
+            eventsAssign(sched);
+        }
         
 
-        for (Slot slot: sched.eventsMap.keySet()) {
-            String key = slot.day;
-            ArrayList<Event> value = sched.eventsMap.get(slot);
-            System.out.print(key + ": [");
-            value.forEach(event-> System.out.print(event.name + ", "));
-            System.out.println("]");
-        }
+        // for (Slot slot: sched.eventsMap.keySet()) {
+        //     String key = slot.day;
+        //     ArrayList<Event> value = sched.eventsMap.get(slot);
+        //     System.out.print(key + ": [");
+        //     value.forEach(event-> System.out.print(event.name + ", "));
+        //     System.out.println("]");
+        // }
         System.out.println(path);
     }
 
-    public void specialAssign(Schedule sched) {
+    private void specialAssign(Schedule sched) {
         // TODO implement specialAssign
     }
     
-    public void partAssign(Schedule sched) {
+    private void partAssign(Schedule sched) {
+        path+= "Part Assign\n";
         input.paMap.forEach((event, slot) ->  {
             assign(sched, event, slot);
         });
     }
 
-    public void assign(Schedule sched, Event event, Slot slot) {
+    private void eventsAssign(Schedule sched) {
+        if (sched.gamesLeft.size() != 0) {
+            path += "Expanding games\n";
+            Event game = sched.gamesLeft.get(0);
+            for (Slot gSlot : gSlots) {
+                Schedule schedCopy = new Schedule(sched);
+                if (assign(schedCopy, game, gSlot) && schedCopy.score <= bestSched.score) 
+                    stack.add(schedCopy);
+            }
+        }
+        else if (sched.pracsLeft.size() != 0) {
+            path += "Expanding pracs\n";
+            Event prac = sched.pracsLeft.get(0);
+            for (Slot pSlot : pSlots) {
+                Schedule schedCopy = new Schedule(sched);
+                if (assign(schedCopy, prac, pSlot) && schedCopy.score <= bestSched.score) 
+                    stack.add(schedCopy);
+            }
+        }
+        else {
+            bestSched = (bestSched.score > sched.score) ? sched : bestSched;
+            path += "Leaf\n";
+        }
+    }  
+
+    // return if assign isValid
+    private Boolean assign(Schedule sched, Event event, Slot slot) {
         // if (eval.isValid(sched, event, slot)) {
         if (true) {
             // calc score
@@ -93,34 +121,41 @@ public class TreeSearch {
             // update gamesLeft/pracsLeft
             if (event.type) sched.gamesLeft.remove(event);
             else sched.pracsLeft.remove(event);
-
+            
             // update path
-            addPath(event, slot, true, sched.score);
-        }
-    }
+            path += "Event: " + event.name + ", Slot: " + slot.day + ", IsValid: true, Score: " + sched.score + "\n";
 
-    public void addPath(Event event, Slot slot, Boolean isValid, int score) {
-        path += "Event: " + event.name + ", Slot: " + slot.day + ", IsValid: " + isValid + ", Score: " + score + "\n";
+            return true;
+        }
+        else path += "Event: " + event.name + ", Slot: " + slot.day + ", IsValid: false\n";
+
+        return false;
     }
 
     public static void main(String[] args) throws Exception {
         Event game1 = new Event("game1", null, null, null, 0, true);
         Event game2 = new Event("game2", null, null, null, 0, true);
         Event game3 = new Event("game3", null, null, null, 0, true);
-        Slot slot1 = new Slot("slot1", null, null, 0, 0, false);
-        Slot slot2 = new Slot("slot2", null, null, 0, 0, false);
-        ArrayList<Slot> slots = new ArrayList<>(Arrays.asList(slot1,slot2));
-        ArrayList<Event> games = new ArrayList<>(Arrays.asList(game1, game2, game3));
+        Event game4 = new Event("game4", null, null, null, 0, true);
+        Event prac1 = new Event("prac1", null, null, null, 0, false);
+        Slot gSlot1 = new Slot("gSlot1", null, null, 0, 0, false);
+        Slot gSlot2 = new Slot("gSlot2", null, null, 0, 0, false);
+        Slot pSlot1 = new Slot("pSlot1", null, null, 0, 0, false);
+        ArrayList<Slot> gSlots = new ArrayList<>(Arrays.asList(gSlot1,gSlot2));
+        ArrayList<Slot> pSlots = new ArrayList<>(Arrays.asList(pSlot1));
+        ArrayList<Event> games = new ArrayList<>(Arrays.asList(game1, game2, game3, game4));
+        ArrayList<Event> pracs = new ArrayList<>(Arrays.asList(prac1));
 
         HashMap<Event, Slot> testPaMap = new HashMap<>();
-        testPaMap.put(game1, slot1);
-        testPaMap.put(game2, slot1);
-        testPaMap.put(game3, slot2);
+        testPaMap.put(game1, gSlot1);
+        testPaMap.put(game2, gSlot1);
     
         Parser input = new Parser("README.md");
         input.paMap = testPaMap;
         input.games = games;
-        input.m_game_slots = slots;
+        input.practices = pracs;
+        input.m_game_slots = gSlots;
+        input.m_prac_slots = pSlots;
         TreeSearch testSearch = new TreeSearch(input);
     }
 }
