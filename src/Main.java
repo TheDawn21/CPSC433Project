f3b0b1646497e6a1bb62798899239f47de53d414
jayoo.hwang1 <jayoo.hwang1@ucalgary.ca>
2022-12-05 17:49:53 -0700

    Main implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        if (args.length == 9) {
+            try {
+                String filename = args[0];
+                // soft constraint weights and penalties
+                int[] weights_penalties = new int[8];
+                for (int i = 1; i<9; i++) {
+                    weights_penalties[i-1] = Integer.parseInt(args[i]);
+                }
+
+                Parser parseObj = new Parser(filename);
+                TreeSearch aTree = new TreeSearch(parseObj);
+                System.out.println(aTree.bestSched.score);
+
+            } catch (NumberFormatException e) {
+                System.err.println("Argument error");
+                System.exit(1);
+            }
+            
+        } else {
+            System.out.println("Invalid input");
+        }
     }
 }
